ece75ec9ba5c9b7e4120125fa83472089e0ccfca
haosimentu <haosimentu@126.com>
2017-07-24 14:16:29 +0800

    Update Base.java

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -7,11 +7,7 @@ class Base {
 
     protected void run() {
         double r = Math.random();
-        if (r < 0.1) {
-            fail("oops");
-        } else if (r < 0.2) {
-            throw new AssumptionViolatedException("skipping");
-        }
+        System.out.println(r);
     }
 
 }
