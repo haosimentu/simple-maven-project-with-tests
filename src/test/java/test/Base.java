545b1904eba2ff39515cac9ed60b7e11c1d55d7b
haosimentu <haosimentu@126.com>
2017-07-25 10:02:26 +0800

    Update Base.java

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -3,7 +3,7 @@ package test;
 import static org.junit.Assert.*;
 import org.junit.internal.AssumptionViolatedException;
 
-class Base {
+public class Base {
 
     protected void run() {
         double r = Math.random();
